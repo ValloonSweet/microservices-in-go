ca240827b446bdde3aaa986bdae98b96f5fe7739
ValloonSweet <valloon91105@gmail.com>
2024-04-24 19:23:26 -0700

    feat: broker service with simple POST handler

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "5000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting broker service on port ", webPort)
+
+	// define htp server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,9 @@
+module broker-service
+
+go 1.21.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+)
